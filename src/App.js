bdf09cd52bbbe87014b804134d4fde0d1c9dd5d0
Kazuto0p <122356200+Kazuto0p@users.noreply.github.com>
2024-04-20 14:05:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import { Route } from 'react-router-dom';
+import './App.css';
+import Admin from './components/Admin';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Admin/>
+     
+
+    </div>
+  );
+}
+
+export default App;
